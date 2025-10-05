8130093db2f20776facd832ea894f63b4d5bc20e
unravel-dev <nikolai.p.ivanov@gmail.com>
2025-10-06 01:50:20 +0300

    [Script]Added Application class.

---
+++ engine_data/data/scripts/system/Application.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Unravel
+{
+namespace Core
+{
+    /// <summary>
+    /// Provides application-level functionality and control.
+    /// </summary>
+    public static class Application
+    {
+        /// <summary>
+        /// Quits the application by stopping playmode.
+        /// This will exit the current play session and return to edit mode.
+        /// </summary>
+        public static void Quit()
+        {
+            internal_m2n_application_quit();
+        }
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private static extern void internal_m2n_application_quit();
+    }
+}
+}
