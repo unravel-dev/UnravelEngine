d5c2a4e26ff65233d4f40bf2fca9f3c2e8437224
unravel-dev <nikolai.p.ivanov@gmail.com>
2026-03-23 00:46:33 +0200

    Fixed incorrect texture creation

--- engine/engine/rendering/default_textures.cpp
+++ engine/engine/rendering/default_textures.cpp
@@ -11,12 +11,22 @@ namespace
 
 auto create_4x4_rgba8(uint8_t r, uint8_t g, uint8_t b, uint8_t a) -> gfx::texture::ptr
 {
-    std::array<uint8_t, 4 * 4> pixel = {r, g, b, a, r, g, b, a, r, g, b, a, r, g, b, a};
-    auto* mem = gfx::copy(pixel.data(), static_cast<uint32_t>(pixel.size()));
+    constexpr uint16_t width = 4;
+    constexpr uint16_t height = 4;
+    constexpr uint16_t num_layers = 1;
+    std::array<uint8_t, width * height * 4> pixels{};
+    for(size_t i = 0; i < width * height; ++i)
+    {
+        pixels[i * 4 + 0] = r;
+        pixels[i * 4 + 1] = g;
+        pixels[i * 4 + 2] = b;
+        pixels[i * 4 + 3] = a;
+    }
+    auto* mem = gfx::copy(pixels.data(), static_cast<uint32_t>(pixels.size()));
     return std::make_shared<gfx::texture>(
-        uint16_t(1), uint16_t(1),
+        width, height,
         false,
-        uint16_t(1),
+        num_layers,
         gfx::texture_format::RGBA8,
         BGFX_TEXTURE_NONE | BGFX_SAMPLER_NONE,
         mem);
