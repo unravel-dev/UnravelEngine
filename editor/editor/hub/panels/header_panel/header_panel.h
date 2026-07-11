6481406d5777b61df51065ff34651b844918cf4b
unravel-dev <nikolai.p.ivanov@gmail.com>
2026-07-11 12:49:43 +0300

    Default false to playing splash screen in editor

--- editor/editor/hub/panels/header_panel/header_panel.h
+++ editor/editor/hub/panels/header_panel/header_panel.h
@@ -36,6 +36,6 @@ private:
 
     imgui_panels* parent_{};
     bool show_about_window_ = false;
-    bool play_splash_in_editor_ = true;
+    bool play_splash_in_editor_ = false;
 };
 } // namespace unravel
